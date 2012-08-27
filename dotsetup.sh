b8919812c4357968316ccb9423bb57374b8089c4
Toshihiro Morimoto <dealforest.net@gmail.com>
2012-08-28 03:16:12 +0900

    add tmux settings

--- dotsetup.sh
+++ dotsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-for file in .zshrc .zshrc.mine .vimrc .vim .gvimrc .screenrc .inputrc .module-starter .gitignore .toprc .sqliterc .proverc .gdbinit .gitconfig .railsrc .gemrc .tigrc
+for file in .zshrc .zshrc.mine .vimrc .vim .gvimrc .screenrc .inputrc .module-starter .gitignore .toprc .sqliterc .proverc .gdbinit .gitconfig .railsrc .gemrc .tigrc .tmux.conf
 do
     rm -v ~/$file
     ln -sv $PWD/$file ~/$file
