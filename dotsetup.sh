53491bf72429187e2a158e9192a7cff863b670ad
Toshihiro Morimoto <dealforest.net@gmail.com>
2013-01-08 21:03:35 +0900

    Add ackrc

--- dotsetup.sh
+++ dotsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-for file in .vimrc .vim .gvimrc .screenrc .inputrc .module-starter .gitignore .toprc .sqliterc .proverc .gdbinit .gitconfig .railsrc .gemrc .tigrc .tmux.conf .pryrc .zsh.d
+for file in .vimrc .vim .gvimrc .screenrc .inputrc .module-starter .gitignore .toprc .sqliterc .proverc .gdbinit .gitconfig .railsrc .gemrc .tigrc .tmux.conf .pryrc .zsh.d .ackrc
 do
     rm -v ~/$file
     ln -sv $PWD/$file ~/$file
