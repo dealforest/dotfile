728f184f53594c5362bf093ba856a303ef0b75d9
Toshihiro Morimoto <dealforest.net@gmail.com>
2014-06-30 18:14:46 +0900

    Prepared to using peco

---
+++ .zsh.d/plugins/peco/peco-cdr.zsh
@@ -0,0 +1,9 @@
+function peco-cdr() {
+  local selected_dir=$(cdr -l | awk '{ print $2 }' | peco)
+  if [ -n "$selected_dir" ]; then
+      BUFFER="cd ${selected_dir}"
+      zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N peco-cdr
